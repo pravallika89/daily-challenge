b35199caa90493a415f52ae9620a4682b75db5b9
pravallika <pravallikaammineni@github.com>
2020-11-03 21:56:33 -0800

    added name and favorite food

---
+++ JS/script.js
@@ -0,0 +1,14 @@
+// Single line comment
+/* Multiple line comment
+var a;
+a=10;
+var b;
+b=20;
+console.log(a);
+console.log(b);*/
+var name;
+name = "pravallika";
+console.log(name);
+var favoriteFood;
+favoriteFood= "Chicken";
+console.log(favoriteFood);
